b3ae655b9d9452e6578a98fd812aea6ae4b66331
FabioTavares3 <fabio.tavares333@hotmail.com>
2025-05-12 01:15:30 -0300

    I've added a new table to the DB and a new insert logic to it. Also added analysis queries that will be useful in the near future

---
+++ Analysis_queries.py
@@ -0,0 +1,87 @@
+top_tracks = """
+SELECT 
+	s.song_name, 
+    s.album_name,
+    s.song_image,
+    s.song_url,
+    date_format(f.dt_listened,"%%M %%Y") month, 
+    count(*) QTT,
+    round(sum(f.DURATION/60000),2) estimated_total_minutes
+FROM
+	f_played f, d_songs s
+WHERE
+	s.song_id = f.SONG_ID
+AND 
+	f.user_id = %s
+GROUP BY 
+	s.song_name, 
+    DATE_FORMAT(f.dt_listened,"%%M %%Y"), 
+    s.album_name,
+	s.song_image,
+    s.song_url
+ORDER BY COUNT(*) DESC
+
+"""
+
+top_artists = """
+SELECT 
+	a.artist_name, 
+    a.artist_image,
+    a.artist_url,
+	DATE_FORMAT(f.dt_listened,"%%M %%Y") month, 
+    COUNT(*) QTT,
+    ROUND(SUM(duration/60000),2) as estimated_total_minutes
+FROM
+	f_played f, d_artists a, d_songs_artists d
+WHERE
+	a.artist_id = d.artist_id
+AND 
+	f.song_id = d.song_id
+AND 
+	f.user_id = %s
+GROUP BY 
+	a.artist_name,
+    DATE_FORMAT(f.dt_listened,"%%M %%Y"),
+    a.artist_image,
+    a.artist_url
+ORDER BY ROUND(SUM(duration/60000),2) desc
+"""
+
+top_albums = """
+SELECT 
+    s.album_name,
+    s.song_image album_image,
+    s.album_url,
+    date_format(f.dt_listened,"%%M %%Y") month, 
+    count(*) quantity,
+    round(sum(f.DURATION/60000),2) estimated_total_minutes
+FROM
+	f_played f, d_songs s
+WHERE
+	s.song_id = f.SONG_ID
+AND 
+	f.user_id = %s
+GROUP BY 
+    DATE_FORMAT(f.dt_listened,"%%M %%Y"), 
+    s.album_name,
+	s.song_image,
+    s.album_url
+ORDER BY COUNT(*) DESC
+"""
+
+
+listening_history = """
+SELECT 
+	f.song_name, a.artist_name, s.album_name, s.song_image, a.artist_image, timestamp
+FROM 
+	f_played f, d_artists a, d_songs s, d_songs_artists d
+WHERE 
+	a.artist_id = d.artist_id
+AND 
+	f.song_id = d.song_id
+AND 
+	d.song_id = s.song_id
+AND
+    f.user_id = %s
+"""
+
--- execution.py
+++ execution.py
@@ -5,6 +5,10 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
 from datetime import datetime
+from Analysis_queries import top_artists,top_albums,top_tracks,listening_history
+
+
+
 
 load_dotenv()
 
@@ -13,6 +17,7 @@ class SpotifyAPI:
     
     
     def authentication(self):
+        # Authenticate with Spotify API using Spotipy
         sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id= os.getenv("CLIENT_ID"),
         client_secret=os.getenv("CLIENT_SECRET"),
@@ -23,6 +28,7 @@ class SpotifyAPI:
 
     
     def get_user_info(self) -> dict:
+        # Get user information from Spotify API
         authentication = self.authentication()
         user_info = authentication.current_user()
         user_id = user_info['id']
@@ -55,20 +61,23 @@ class DBconnector:
     
 
     def get_connection(self):
+        # Create a connection to the MySQL database using SQLAlchemy
         return create_engine(
             url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(
                 os.getenv("user"), os.getenv("password"), os.getenv("host"), 3306, os.getenv("database"))
             )
     
     def execute_query(self,query, params=None) -> None:
-        
+        # Execute a query and return the result as a pandas DataFrame
         if params:
             result = pd.read_sql(query, self.get_connection(), params=params)
         else:
             result = pd.read_sql(query, self.get_connection())
         
         return result
+    
     def insert_data(self, table, data) -> None:
+        # Insert data into the specified table
         try:
             data.to_sql(table, con=self.get_connection(), if_exists='append', index=False)
         except Exception as e:
@@ -84,6 +93,7 @@ class DBconnector:
             return True
     
     def insert_d_songs(self): 
+        # For each song in the recently played list, we need to insert the respective records into the d_songs table
         for song in self.get_songs['items']:
             track= song['track']
             d_songs_data = {
@@ -97,14 +107,17 @@ class DBconnector:
                 "album_url": track['album']['external_urls']['spotify'],
                 "album_id": track['album']['id']
     }
+            # Check if the song_id already exists in the d_songs table if not, insert them
             if not self.does_song_exist(d_songs_data['song_id']):
                 
                 d_songs_df = pd.DataFrame(d_songs_data,index = [0])
                 self.insert_data('d_songs', d_songs_df)
         
     def does_artist_exist(self, artist_id: str) -> bool:
+        # Check if the artist_id already exists in the d_artists table
         query = "SELECT 1 FROM d_artists WHERE artist_id = %s LIMIT 1"
         result = self.execute_query(query, (artist_id,))
+        # If the result is empty, it means the artist_id does not exist in the d_artists table
         if result.empty:
             return False
         else:
@@ -112,6 +125,7 @@ class DBconnector:
 
 
     def insert_d_artists(self):
+        # For each song in the recently played list, we need to insert the respective records into the d_artists table
         for song in self.get_songs['items']:
             track = song['track']
             for artist in track['artists']:
@@ -124,15 +138,17 @@ class DBconnector:
                     "artist_uri": artist_details['uri'],
                     "artist_image": artist_details['images'][0]['url'] if artist_details['images'] else None
                 }
-               
+                # Check if the artist_id already exists in the d_artists table if not, insert them
                 if not self.does_artist_exist(d_artists_data['artist_id']):
                     d_artists_df = pd.DataFrame(d_artists_data, index=[0])
                     self.insert_data('d_artists', d_artists_df)
     
 
     def does_user_exist(self, user_id: str) -> bool:
+        # Check if the user_id already exists in the d_user table
         query = "SELECT 1 FROM d_user WHERE user_id = %s LIMIT 1"
         result = self.execute_query(query, (user_id,))
+        # If the result is empty, it means the user_id does not exist in the d_user table
         if result.empty:
             return False
         else:
@@ -140,35 +156,81 @@ class DBconnector:
 
     
     def insert_d_user(self):
-        user_info = self.get_user_info
-        user_info_df = pd.DataFrame(user_info, index=[0])
+        user_info_df = pd.DataFrame(self.get_user_info, index=[0])
+        # Check if the user already exists in the d_user table if not, insert them
         if not self.does_user_exist(user_info_df['user_id'].iloc[0]):
             self.insert_data('d_user', user_info_df)
 
 
+
     def insert_f_played(self):
+        # For each song in the recently played list, we need to insert the respective records into the f_played table
         for song in self.get_songs['items']:
             track = song['track']
             dt = datetime.strptime(song['played_at'].replace("Z", ""), "%Y-%m-%dT%H:%M:%S.%f")
             date = dt.strftime("%Y-%m-%d %H:%M:%S")
+            date2 = dt.strftime("%Y-%m-%d")
             f_played_data = {
                 "song_id": track['id'],
                 "song_name": track['name'],
-                "artist_id": track['artists'][0]['id'],
                 "user_id": self.get_user_info['user_id'],
                 "timestamp": date,
-                "duration": track['duration_ms']
+                "duration": track['duration_ms'],
+                "dt_listened": date2
             }
             f_played_df = pd.DataFrame(f_played_data, index=[0])
             self.insert_data('f_played', f_played_df)
 
 
+    def is_song_stored(self,song_id: str,artist_id: str) -> bool:
+        # Check if the song_id and artist_id already exist in the junction table
+        query = "SELECT 1 FROM d_songs_artists WHERE song_id = %s AND artist_id = %s LIMIT 1"
+        result = self.execute_query(query, (song_id, artist_id))
+        # If the result is empty, it means the song_id and artist_id do not exist in the junction table
+        if result.empty:
+            return False
+        else:
+            return True
+
+    def insert_junction_table(self):
+        # For each song in the recently played list, we need to insert the song_id and artist_id into the junction table
+        for song in self.get_songs['items']:
+            track = song['track']
+            for artist in track['artists']:
+                d_songs_data = {
+                    "song_id": track['id'],
+                    "artist_id": artist['id']
+                }
+                # Check if the song_id and artist_id already exist in the junction table if not, insert them
+                if not self.is_song_stored(d_songs_data['song_id'], d_songs_data['artist_id']):
+    
+                    d_songs_df = pd.DataFrame(d_songs_data, index=[0])
+                    self.insert_data('d_songs_artists', d_songs_df)
+
     def insert_records(self):
         self.insert_d_songs()
         self.insert_d_artists()
+        self.insert_junction_table()
         self.insert_d_user()
         self.insert_f_played()
-        return "Records inserted successfully"
+        print("Records inserted successfully")
     
 
+    #Analysis queries functions. They simply return the result of the query
+    # to be used in the analysis notebook.
+    def get_history_in_DB(self):
+        return self.execute_query(listening_history, (self.get_user_info['user_id'],))
 
+    def get_top_artists_in_DB(self):
+        return self.execute_query(top_artists, (self.get_user_info['user_id'],))
+    
+    def get_top_albums_in_DB(self):
+        return self.execute_query(top_albums, (self.get_user_info['user_id'],))
+    
+    def get_top_tracks_in_DB(self):
+        return self.execute_query(top_tracks, (self.get_user_info['user_id'],))
+
+
+if __name__ == "__main__":
+    conn = DBconnector()
+    conn.insert_records()
